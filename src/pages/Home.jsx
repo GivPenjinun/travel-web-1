75108e41c7be4c4c310bd99963a43db34926c5b5
GivPenjinun <penjinun.watt@gmail.com>
2023-08-01 23:08:38 +0700

    feature: home page v1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,103 +1,25 @@
 import { useState, useEffect } from "react";
 import "./Home.css";
 import { Link, useNavigate } from "react-router-dom";
-import { FaSearch } from "react-icons/fa";
-import { BsFillPersonFill } from "react-icons/bs";
-import { ImCross } from "react-icons/im";
-import { AiOutlineBars } from "react-icons/ai";
+import { Header } from "./Header";
+import video from "../assets/video.mp4";
+import video2 from "../assets/video2.mp4";
+import video3 from "../assets/video3.mp4";
+import video4 from "../assets/video4.mp4";
+import video5 from "../assets/video5.mp4";
 
 function Home() {
-  const [isBtnToggle, setBtnToggle] = useState(true);
-  const [barToggle, setBarToggle] = useState("search-bar-container");
-  const [logInToggle, setLogInToggle] = useState("login-form-container");
-  const [menuToggle, setMenuToggle] = useState("navBar");
+  const [toggleVideo, setToggleVideo] = useState();
+  const [currentCase, setCurrentCase] = useState(0);
+  const [vid, setVideo] = useState(video);
 
-  const handlerBtn = () => {
-    if (isBtnToggle == true) {
-      setBtnToggle(false);
-      setBarToggle("search-bar-container active");
-    } else {
-      setBtnToggle(true);
-      setBarToggle("search-bar-container");
-    }
-  };
-
-  const handlerLogIn = () => {
-    setLogInToggle("login-form-container active");
-  };
-
-  const handlerCloseLogIn = () => {
-    setLogInToggle("login-form-container");
-  };
-
-  const handlerMenuToggle = () => {
-    if (menuToggle === "navBar") {
-      setMenuToggle("navBar active");
-    } else {
-      setMenuToggle("navBar");
-    }
-  };
+  function currentSelector(e) {
+    setCurrentCase(Number(e.target.value));
+  }
 
   return (
     <>
-      <header>
-        <button id="menu-bar" onClick={handlerMenuToggle}>
-          <AiOutlineBars />
-        </button>
-        <a href="#" className="logo">
-          <span>T</span>ravel
-        </a>
-        <nav className={menuToggle}>
-          <a href="#Home">Home</a>
-          <a href="#Book">Book</a>
-          <a href="#Packages">Packages</a>
-          <a href="#Services">Services</a>
-          <a href="#Gallery">Gallery</a>
-          <a href="#Review">Review</a>
-          <a href="#Contact">Contact</a>
-        </nav>
-
-        <button className="icons" onClick={handlerBtn}>
-          {isBtnToggle ? (
-            <FaSearch id="search-btn" />
-          ) : (
-            <ImCross id="search-btn" />
-          )}
-        </button>
-        <button className="icons" onClick={handlerLogIn}>
-          <BsFillPersonFill id="login-btn" />
-        </button>
-
-        <form action="" className={barToggle}>
-          <input type="search" id="search-bar" placeholder="search here..." />
-          <label htmlFor="search-bar">
-            <FaSearch id="search-btn" />
-          </label>
-        </form>
-      </header>
-      <div className={logInToggle}>
-        <button onClick={handlerCloseLogIn}>
-          <ImCross id="form-close" />
-        </button>
-        <form action="">
-          <h3>Login</h3>
-          <input type="email" className="box" placeholder="enter your email" />
-          <input
-            type="password"
-            className="box"
-            placeholder="enter your passwword"
-          />
-          <input type="submit" value="login now" className="btn" />
-          <input type="checkbox" id="remember" />
-          <label htmlFor="remember">Remember Me</label>
-          <p>
-            forget passwword? <a href="#">click here</a>
-          </p>
-          <p>
-            don't have an account? <a href="#">register now</a>
-          </p>
-        </form>
-      </div>
+      <Header />
       <section className="home" id="home">
         <div className="content">
           <h3>adventure is waiting...</h3>
@@ -105,6 +27,28 @@ function Home() {
           <a href="#" className="btn">
             discover more
           </a>
+        </div>
+
+        <div className="controls">
+          <button
+            value={0}
+            className={currentCase === 0 ? "vid-btn active" : "vid-btn"}
+            data-src={video}
+            onClick={currentSelector}
+          ></button>
+          <button
+            value={1}
+            className={currentCase === 1 ? "vid-btn active" : "vid-btn"}
+            data-src={video2}
+            onClick={currentSelector}
+          ></button>
+          <button className="vid-btn" data-src={video3}></button>
+          <button className="vid-btn" data-src={video4}></button>
+          <button className="vid-btn" data-src={video5}></button>
+        </div>
+
+        <div className="video-container">
+          <video src={video} id="video-slider" loop autoPlay muted></video>
         </div>
       </section>
     </>
