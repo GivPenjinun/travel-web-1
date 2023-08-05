4c73cfaaded70edc6ae25a2971eeb34d482c15f8
GivPenjinun <penjinun.watt@gmail.com>
2023-08-05 22:12:24 +0700

    feat: add packages

--- src/pages/Packages.jsx
+++ src/pages/Packages.jsx
@@ -1,9 +1,42 @@
 import "./Packages.css";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+import { ImLocation2 } from "react-icons/im";
 
 function Packages() {
   return (
     <>
-      <section className="packages" id="packages"></section>
+      <section className="packages" id="packages">
+        <h1 className="heading">
+          <span>P</span>
+          <span>A</span>
+          <span>C</span>
+          <span>K</span>
+          <span>A</span>
+          <span>G</span>
+          <span>E</span>
+          <span>S</span>
+        </h1>
+        <div className="box-container">
+          <div className="box">
+            <img src="" alt="" />
+            <div className="content">
+              <h3>
+                <ImLocation2 className="icon" />
+                mumbai
+              </h3>
+              <p>Lorem</p>
+              <div className="stars">
+                <AiFillStar className="icon" />
+                <AiOutlineStar className="icon" />
+              </div>
+              <div className="price">
+                $90.00<span className="discount">$120.00</span>
+              </div>
+              <button className="btn">Book Now</button>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
