7085fb6833a086fd24b6b3a0e61f5759ab530ed3
GivPenjinun <penjinun.watt@gmail.com>
2023-08-06 16:13:48 +0700

    feat: add service section

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
       <Home />
       <Book />
       <Packages />
+      <Service />
     </div>
   );
 }
--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -1,9 +1,90 @@
 import "./Service.css";
+import {
+  BiSolidHotel,
+  BiSolidMegaphone,
+  BiWorld,
+  BiSolidPlaneAlt,
+} from "react-icons/bi";
+import { ImSpoonKnife } from "react-icons/im";
+import { GiBackpack } from "react-icons/gi";
 
 function Service() {
   return (
     <>
-      <section className="service" id="servive"></section>
+      <section className="services" id="services">
+        <h1 className="heading">
+          <span>S</span>
+          <span>E</span>
+          <span>R</span>
+          <span>V</span>
+          <span>I</span>
+          <span>C</span>
+          <span>E</span>
+          <span>S</span>
+        </h1>
+        <div className="box-container">
+          <div className="box">
+            <BiSolidHotel id="icon" />
+            <h3>Affordable Hotels</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut optio
+              nostrum cupiditate doloribus unde labore repellendus,
+              reprehenderit aperiam asperiores vitae sit eligendi culpa. Cumque,
+              perferendis? Omnis sapiente minus possimus sequi?
+            </p>
+          </div>
+          <div className="box">
+            <ImSpoonKnife id="icon" />
+            <h3>Foods And Drinks</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut optio
+              nostrum cupiditate doloribus unde labore repellendus,
+              reprehenderit aperiam asperiores vitae sit eligendi culpa. Cumque,
+              perferendis? Omnis sapiente minus possimus sequi?
+            </p>
+          </div>
+          <div className="box">
+            <BiSolidMegaphone id="icon" />
+            <h3>Safety Guides</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut optio
+              nostrum cupiditate doloribus unde labore repellendus,
+              reprehenderit aperiam asperiores vitae sit eligendi culpa. Cumque,
+              perferendis? Omnis sapiente minus possimus sequi?
+            </p>
+          </div>
+          <div className="box">
+            <BiWorld id="icon" />
+            <h3>Around The World</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut optio
+              nostrum cupiditate doloribus unde labore repellendus,
+              reprehenderit aperiam asperiores vitae sit eligendi culpa. Cumque,
+              perferendis? Omnis sapiente minus possimus sequi?
+            </p>
+          </div>
+          <div className="box">
+            <BiSolidPlaneAlt id="icon" />
+            <h3>Fastest Travel</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut optio
+              nostrum cupiditate doloribus unde labore repellendus,
+              reprehenderit aperiam asperiores vitae sit eligendi culpa. Cumque,
+              perferendis? Omnis sapiente minus possimus sequi?
+            </p>
+          </div>
+          <div className="box">
+            <GiBackpack id="icon" />
+            <h3>Adventures</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut optio
+              nostrum cupiditate doloribus unde labore repellendus,
+              reprehenderit aperiam asperiores vitae sit eligendi culpa. Cumque,
+              perferendis? Omnis sapiente minus possimus sequi?
+            </p>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
